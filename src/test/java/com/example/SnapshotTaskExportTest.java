ed2633ecca5cf6c6d04b30c1fc7576e396d01284
KoheiHara <3kzey8c9gd7@gmail.com>
2025-08-13 18:26:55 +0900

    chore: add drop

---
+++ src/test/java/com/example/SnapshotTaskExportTest.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.jboss.weld.junit5.WeldInitiator;
+import org.jboss.weld.junit5.WeldSetup;
+import org.junit.jupiter.api.Test;
+
+import com.github.database.rider.junit5.api.DBRider;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.exporter.ExportDataSet;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import com.example.task.Task;
+import com.example.task.mybatis.TaskMapper;
+
+@DBRider
+class SnapshotTaskExportTest extends BaseTest {
+
+    @WeldSetup
+    WeldInitiator weld = WeldInitiator.from(
+        Arrays.stream(WELD_CORE_BEANS).toArray(Class[]::new)
+    ).activate(RequestScoped.class).build();
+
+    @Inject
+    TaskMapper taskMapper;
+
+    @Test
+    @DataSet(value = "datasets/tasks.yml", cleanBefore = true, cleanAfter = true)
+    @ExportDataSet(outputName = "./tasks-after-export.yml")
+    void export_afterInsert() {
+        Task t = new Task();
+        t.setTitle("SnapshotTask");
+        t.setDueDate(LocalDate.now());
+        t.setCompleted(false);
+        taskMapper.create(t);
+        System.out.println(taskMapper.findAll());
+    }
+}
